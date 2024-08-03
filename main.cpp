ba47dfdc42942898e447539204216097d58a29b4
diego4lbarracin <diegoalbarracin0405@gmail.com>
2024-08-03 10:18:14 -0500

    Update: Version nueva.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int functionHola(int x);
+float retonarFlotante(float y);
 int main()
 {
 
     cout << "Blablba" << endl;
     cout << functionHola(31) << endl;
+    cout << retonarFlotante(3.36) << endl;
     return 0;
 }
 
 int functionHola(int x)
 {
     return x + 19;
+}
+
+float retonarFlotante(float y)
+{
+    return y * 10.6;
 }
